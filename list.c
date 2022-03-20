287959ea5053f3814cd18b775c2680829b810889
Your Name <you@example.com>
2022-03-20 20:27:46 -0300

    new try

--- list.c
+++ list.c
@@ -86,6 +86,19 @@ void pushBack(List * list, void * data) {
 }
 
 void pushCurrent(List * list, void * data) {
+    assert(list != NULL && list -> current != NULL);
+    Node * new = createNode(data);
+
+    if(list -> current -> next){
+        new -> prev = list -> current;}
+    new -> prev = list -> current;
+
+    if(list -> current -> next){
+        list -> current -> next -> prev = new;}
+    list -> current -> next = new;
+
+    if (list -> current == list -> tail) list -> tail = new;
+
 }
 
 void * popFront(List * list) {
